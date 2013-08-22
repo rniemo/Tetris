f134c5d466e7b5e3dad3ad4bc927ca2919993e03
Daniel Marchese <marchese.daniel@gmail.com>
2013-08-22 12:01:31 -0400

    Initial code submission

---
+++ src/GameLoop.java
@@ -0,0 +1,18 @@
+
+/**
+ * Contains all of the necessary mechanisms for running
+ * the game.
+ */
+public class GameLoop {
+
+    /**
+     * The main method
+     * 
+     * @param args
+     *          The command line arguments, unused here.
+     */
+    public static void main(String[] args) {
+        
+    }
+}
+
